14ea22eaa24b12bc77ff7869698d7bf74dff32ca
EthanWillCode <Aethil@yahoo.com>
2023-09-27 21:48:05 -0500

    Wrote Mission Statement Page

---
+++ src/AboutUs/Mission.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Mission(){
+    return (
+        <div>
+            <h2>Our Mission</h2>
+            <p>
+                One third of Americans get less than seven hours of sleep.
+                Our goal, put simply, is to help you get to sleep faster.
+                We will do this by adjusting the tempo of audio tracks to guide you biorhythms to a gentle snooze.
+            </p>
+        </div>
+    )
+}
