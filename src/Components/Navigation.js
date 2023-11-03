fa911799dc524f0b7327624d200fd343b67ae88b
EthanWillCode <Aethil@yahoo.com>
2023-11-02 19:30:18 -0500

    building out navbar

---
+++ src/Components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navigation(props){
+
+    return (
+        <nav>
+            <ul>
+                <a><li></li></a>
+                <a><li></li></a>
+                <a><li></li></a>
+            </ul>
+        </nav>
+    )
+}
